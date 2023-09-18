88fdc55da266c8754d3d421697bc5d625ea1e61a
Joan David Plazas <112639563+joan-plazas@users.noreply.github.com>
2023-09-18 12:25:59 -0500

    Delete portfolio-dev directory

--- portfolio-dev/assets/index.js
+++
@@ -1,5 +0,0 @@
-
-function tooltip() {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-}
