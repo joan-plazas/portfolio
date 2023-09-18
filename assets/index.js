0b246ce903325e0fe5186ae9ef3bc2a8ec3d2590
Joan David Plazas <112639563+joan-plazas@users.noreply.github.com>
2023-09-18 14:11:07 -0500

    Portfolio v1.0.0

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+
+function tooltip() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
